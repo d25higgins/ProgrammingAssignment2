ab87a917b73a60c32c4ff63205eabe9c1a4b6db8
d25higgins <d25higgins@gmail.com>
2014-12-06 15:54:47 -0800

    Finished Assignment
    Finished code for Assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## ==================================================================
+## ProgrammingAssignment #2
+## ==================================================================
+## Author: d25higgins
+## Incept: Dec 6, 2014
+## Description:
+## ==================================================================
+## Two functions for inverting and caching a matrix in
+## order to speed repetative computations.
+## ==================================================================
+## Edit History:
+##
+## ==================================================================
 
-## Write a short comment describing this function
+## function to create a list of functions to manipulate a matrix (invert)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    im <- NULL
+    set <- function(y){
+        x <<- y
+        im <<- NULL
+    }
+    get <- function(){x}
+    setInverse <- function(solve){im <<- solve}
+    getInverse <- function(){im}
+    list(set = set, get = get, setInverse = setInverse, 
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## function to return cached inverted matrix or to create one and save it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    im <- x$getInverse()
+    if(!is.null(im)){
+        message("getting inverse matrix")
+        return(im)
+    }
+    data <- x$get()
+    im <- solve(data, ...)
+    x$setInverse(im)
+    im
 }
